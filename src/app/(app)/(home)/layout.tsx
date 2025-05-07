1c4daba9d21158e1a7d559b12434abc5d253c986
superAggressiveCoder2026 <parkarsidra01@gmail.com>
2025-05-07 19:05:54 +0530

    third commit

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,24 +1,50 @@
+import { Category } from '@/payload-types';
+import configPromise from '@payload-config';
+import { getPayload } from 'payload';
 
-import { Navbar } from "./navbar";
-import{Footer} from "./footer";
+import { Navbar } from './navbar';
+import { Footer } from './footer';
+import { SearchFilters } from './search-filters';
 
 interface Props {
-    children: React.ReactNode;
-  };
-  
-  const Layout = ({ children }: Props) => {
-    return (
-      <div className="flex flex-col min-h-screen">
-           <Navbar/>
-           <div className="flex-1 bg-[#F4F4F0]">
-           {children}
-           </div>
-            
-            <Footer/>
-            
-      </div>
-    );
-  }
-  
-  export default Layout;
-  
+  children: React.ReactNode;
+}
+
+const Layout = async ({ children }: Props) => {
+  const payload = await getPayload({ config: configPromise });
+
+  const data = await payload.find({
+    collection: 'categories',
+    depth: 1,
+    pagination: false,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      ...(doc as Category),
+      subcategories:undefined
+    }))
+  }));
+
+  console.log({
+    data,
+    formattedData,
+  });
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <SearchFilters data={formattedData} />
+      <div className="flex-1 bg-[#F4F4F0]">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,17 +1,10 @@
-import configPromise from '@payload-config'
-import {getPayload} from 'payload'
 
 export default async function Home() {
-  const payload = await getPayload({
-    config: configPromise,
-  })
-  const data = await payload.find({
-    collection: "categories",
-  })
+ 
 
   return (
     <div>
-      {JSON.stringify(data,null,2)}
+     Home page
     </div>
     
    
